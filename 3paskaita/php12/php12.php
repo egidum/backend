4eb7e1d227f14b912e3a8d43057c7b4164162a47
egidum <sixpilot@yahoo.com>
2019-08-15 15:29:07 +0300

    uzduotis (patikrinimas su empty; rezultatas be formos)

--- 3paskaita/php12/php12.php
+++ 3paskaita/php12/php12.php
@@ -6,33 +6,40 @@
      <title></title>
   </head>
   <body>
-      <form action="" method="post">
-         <h4>Įveskite savo duomenys:</h4>
-         <div>
-            <p style="margin-bottom: 0">Vardas:</p>
-            <input type="text" name="fname">
-         </div>
-         <div>
-            <p style="margin-bottom: 0">Pavardė:</p>
-            <input type="text" name="lname">
-         </div>
-         <div>
-            <p></p>
-            <input type="submit" value="Pateikti" name="submit">
-         </div>
+     <div class="form">
+         <?php
+         $displayForm = True;
+         if(isset($_POST['submit'])) {
+         if(empty($_POST['fname'] && $_POST['lname'])){
+         die("<p>Būtina užpildyti visus laukus!</p>");
+         }
+         echo("<p>Prisijungėte kaip, {$_POST['fname']} {$_POST['lname']}</p>");
+         $displayForm = False;
+         }
+         if($displayForm) {
+         ?>
 
+         <form action="" method="post">
+            <h4>Įveskite savo duomenys:</h4>
+            <div>
+               <p style="margin-bottom: 0">Vardas:</p>
+               <input type="text" name="fname">
+            </div>
+            <div>
+               <p style="margin-bottom: 0">Pavardė:</p>
+               <input type="text" name="lname">
+            </div>
+            <div>
+               <p></p>
+               <input type="submit" value="Pateikti" name="submit">
+            </div>
+         </form>
 
          <?php
-            if(empty($_POST['fname'] && $_POST['lname'])){
-               die("<p>Būtina užpildyti visus laukus!</p>");
-            }
-            if(isset($_POST['submit'])) {
-               echo("<p>Prisijungėte kaip, {$_POST['fname']} {$_POST['lname']}</p>");
-            }
-            exit;
+         }
+         exit;
          ?>
-      </form>
-
+      </div>
   </body>
 </html>
 

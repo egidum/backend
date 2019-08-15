4c5e4f3661467104ff25cbc8ef95edc4b3cd5f42
egidum <sixpilot@yahoo.com>
2019-08-15 20:50:27 +0300

    uzduotis (rezultatas be formos)

--- 3paskaita/php11/php11.php
+++ 3paskaita/php11/php11.php
@@ -8,26 +8,32 @@ naršyklę.-->
      <title></title>
   </head>
   <body>
-      <form action="" method="post">
-         <h4>Įveskite stačiakampio ilgį ir plotį (metrais):</h4>
-         <p style="color:#ff0000">
-         Svarbu! Skaičiai po kablelio įvedami su tašku
-         </p>
-         ilgis: <input type="text" name="ilgis">
-         plotis: <input type="text" name="plotis">
-         <input type="submit" value="skaičiuoti" name="submit"><br>
+     <div class="form">
+         <?php
+         $displayForm = True;
+         if($_POST['ilgis'] && $_POST['plotis']) {
+         $plotas = $_POST['ilgis'] * $_POST['plotis'];
+         echo "<p>Stačiakampio plotas: $plotas kv.m</p>";
+         $displayForm = False;
+         }
+         if($displayForm) {
+         ?>
+
+         <form action="" method="post">
+            <h4>Įveskite stačiakampio ilgį ir plotį (metrais):</h4>
+            <p style="color:#ff0000">
+            Svarbu! Skaičiai po kablelio įvedami su tašku
+            </p>
+            ilgis: <input type="text" name="ilgis">
+            plotis: <input type="text" name="plotis">
+            <input type="submit" value="skaičiuoti" name="submit"><br>
+         </form>
 
          <?php
-            if(empty($_POST['ilgis'] && $_POST['plotis'])){
-               die("<p>Įveskite duomenys!</p>");
-            }
-            if(isset($_POST['submit'])) {
-               $plotas = $_POST['ilgis'] * $_POST['plotis'];
-               echo "<p>Stačiakampio plotas: $plotas kv.m</p>";
-            }
-            exit;
+         }
+         exit;
          ?>
-      </form>
 
+     </div>
   </body>
 </html>

8f1bae05989b70129cbbd320d9eb83ed4609a8be
egidum <sixpilot@yahoo.com>
2019-08-21 14:49:13 +0300

    uzduotis (supaprastinta)

--- 3paskaita/php11/index11.php
+++ 3paskaita/php11/index11.php
@@ -12,16 +12,12 @@ laukelius ir paspaudė mygtuką.-->
   <body>
      <div class="form">
          <?php
-         $displayForm = True;
          if($_POST['ilgis'] && $_POST['plotis']) {
          $plotas = $_POST['ilgis'] * $_POST['plotis'];
          echo "<p>Stačiakampio plotas: $plotas kv.m</p>";
-         $displayForm = False;
-         }
-         if($displayForm) {
-         ?>
-
-         <form action="" method="post">
+         } else {
+            echo '
+            <form action="" method="post">
             <h4>Įveskite stačiakampio ilgį ir plotį (metrais):</h4>
             <p style="color:#ff0000">
             Svarbu! Skaičiai po kablelio įvedami su tašku
@@ -29,13 +25,11 @@ laukelius ir paspaudė mygtuką.-->
             ilgis: <input type="text" name="ilgis">
             plotis: <input type="text" name="plotis">
             <input type="submit" value="skaičiuoti" name="submit"><br>
-         </form>
-
-         <?php
+            </form>
+            ';
          }
          exit;
          ?>
-
      </div>
   </body>
 </html>

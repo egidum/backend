788f8650c46a8e94e00afbd33b60d34ee755380f
egidum <sixpilot@yahoo.com>
2019-08-21 17:20:53 +0300

    uzduotis (supaprastinta)

--- 3paskaita/php12/index12.php
+++ 3paskaita/php12/index12.php
@@ -2,24 +2,22 @@
 - Sukurkite formą, kuri leis vartotojui įvesti savo vardą ir pavardę. Paspaudus mygtuką, šie duomenys turėtų būti išspausdinti naršyklėje. Forma turėtų dingti, kai išvedamas rezultatas, o rezultato spausdinimas turėtų būti tik tuomet, kai vartotojas užpildė visus laukelius ir paspaudė mygtuką.-->
 
 <html>
-  <head>
-     <title></title>
-  </head>
-  <body>
-     <div class="form">
-         <?php
-         $displayForm = True;
-         if(isset($_POST['submit'])) {
-         if(empty($_POST['fname'] && $_POST['lname'])){
-         die("<p>Būtina užpildyti visus laukus!</p>");
-         }
-         echo("<p>Prisijungėte kaip, {$_POST['fname']} {$_POST['lname']}</p>");
-         $displayForm = False;
-         }
-         if($displayForm) {
-         ?>
 
-         <form action="" method="post">
+<head>
+   <title></title>
+</head>
+
+<body>
+   <div class="form">
+      <?php
+       if(!empty($_POST['fname']) && !empty($_POST['lname'])) {
+          if(!preg_match("/^[A-Za-z]*$/", $_POST['fname']) || !preg_match("/^[A-Za-z]*$/", $_POST['lname'])) {
+             die("<p>Neteisingai įvestas vardas!</p>");
+          }
+         echo '<p>Prisijungėte kaip, ' . $_POST['fname'] . ' ' . $_POST['lname'] . '</p>';
+       } else {
+            echo '
+            <form action="" method="post">
             <h4>Įveskite savo duomenys:</h4>
             <div>
                <p style="margin-bottom: 0">Vardas:</p>
@@ -33,14 +31,13 @@
                <p></p>
                <input type="submit" value="Pateikti" name="submit">
             </div>
-         </form>
+            </form>
+            ';
+       }
 
-         <?php
-         }
-         exit;
-         ?>
-      </div>
-  </body>
+      exit;
+      ?>
+   </div>
+</body>
+
 </html>
-
-

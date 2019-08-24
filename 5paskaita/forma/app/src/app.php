8e54d2978578ce2f31801f632e04dfcad3f9e1ee
egidum <sixpilot@yahoo.com>
2019-08-24 12:31:35 +0300

    paskaitos

--- 5paskaita/forma/app/src/app.php
+++ 5paskaita/forma/app/src/app.php
@@ -53,23 +53,22 @@
    */
 
    //Formos siuntimas
-   $vardas = trim($_POST['vardas']);
-   $email = trim($_POST['email']);
-   $message = trim($_POST['message']);
-
-   if(!empty($vardas) && !empty($email) && !empty($message)) {
-      if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
-         $from = "$email";
-         $to = "andrius@vilniuscoding.lt";
-         $subject = "Nauja žinutė";
-         $autorius = 'Nuo:' . $vardas . ', ' . $email;
-         $zinute = htmlspecialchars($message);
-         //mail($to, $subject, $autorius, $zinute, $from);
-         echo "<script>alert('Dėkojame. Jūsų žinutė gauta. Netrukus susisieksime.');</script>";
-      }
-   }
-
-   include('db.php');
    if(isset($_POST['submit'])) {
+      $vardas = trim($_POST['vardas']);
+      $email = trim($_POST['email']);
+      $message = trim($_POST['message']);
 
+      if(!empty($vardas) && !empty($email) && !empty($message)) {
+         if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $from = "$email";
+            $to = "andrius@vilniuscoding.lt";
+            $subject = "Nauja žinutė";
+            $autorius = 'Nuo:' . $vardas . ', ' . $email;
+            $zinute = htmlspecialchars($message);
+            //mail($to, $subject, $autorius, $zinute, $from);
+            echo "<script>alert('Dėkojame. Jūsų žinutė gauta. Netrukus susisieksime.');</script>";
+         }
+      }
+
+      include('db.php');
    }
